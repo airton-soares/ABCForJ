399fe57d7a7d76af10502457814f5549d753154a
Airton Soares da Silva Júnior <assj@ecomp.poli.br>
2017-04-19 11:30:20 -0300

    Commit inicial do projeto
    - Adicionando a pasta do projeto Java no repositório

---
+++ abcforj/src/main/java/com/github/abcforj/Main.java
@@ -0,0 +1,13 @@
+package com.github.abcforj;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
