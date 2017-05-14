109178e26c83894f2cef4ee82529b1126c4fe031
Airton Soares <assj@ecomp.poli.br>
2017-05-13 23:43:00 -0300

    Intesificação não paralela
    - Adicionada a intensificação da busca de forma sequencial.

--- abcforj/src/main/java/com/github/abcforj/bee/ScoutBee.java
+++ abcforj/src/main/java/com/github/abcforj/bee/ScoutBee.java
@@ -9,6 +9,7 @@ import com.github.abcforj.function.Function;
 public class ScoutBee
 {
     private double[] currentPosition;
+    private double[] bestFoodSourcePosition;
     private List<OnlookerBee> allocatedOnlookerBees;
 
     public ScoutBee(double bottomDomainLimit, double topDomainLimit, int dimension)
@@ -20,6 +21,7 @@ public class ScoutBee
 	    this.currentPosition[i] = bottomDomainLimit + Math.random() * (topDomainLimit - bottomDomainLimit);
 	}
 
+	this.bestFoodSourcePosition = this.currentPosition;
 	this.allocatedOnlookerBees = new ArrayList<OnlookerBee>();
     }
 
@@ -28,38 +30,87 @@ public class ScoutBee
 
     }
 
-    public void updateAllocatedOnlookerBeesPositions(Function function)
+    public void updateAllocatedOnlookerBeesPositions(Function function, int limitOfExploitationCycles)
     {
 	int allocatedOnlookerBeesSize = this.allocatedOnlookerBees.size();
-
-	for (int i = 0; i < allocatedOnlookerBeesSize; i++)
+	int limitOfExploitationCyclesAux = limitOfExploitationCycles;
+	
+	while (limitOfExploitationCycles > 0)
 	{
-	    OnlookerBee currentOnlookerBee = this.allocatedOnlookerBees.get(i);
-	    double currentFitness = function.fitness(currentOnlookerBee.getCurrentPosition());
-
-	    int neighborIndex = 0;
-
-	    do
+	    for (int i = 0; i < allocatedOnlookerBeesSize; i++)
 	    {
-		neighborIndex = ThreadLocalRandom.current().nextInt(0, allocatedOnlookerBeesSize);
-	    } while (neighborIndex == i);
+		OnlookerBee currentOnlookerBee = this.allocatedOnlookerBees.get(i);
+		double currentFitness = function.fitness(currentOnlookerBee.getCurrentPosition());
 
-	    OnlookerBee neighborOnlookerBee = this.allocatedOnlookerBees.get(neighborIndex);
+		int neighborIndex = 0;
 
-	    for (int j = 0; j < currentOnlookerBee.getCurrentPosition().length; j++)
-	    {
-		currentOnlookerBee.getCurrentPosition()[j] += ThreadLocalRandom.current().nextInt(-1, 2)
-			* (currentOnlookerBee.getCurrentPosition()[j] - neighborOnlookerBee.getCurrentPosition()[j]);
+		do
+		{
+		    neighborIndex = ThreadLocalRandom.current().nextInt(0, allocatedOnlookerBeesSize);
+		} while (neighborIndex == i);
+
+		OnlookerBee neighborOnlookerBee = this.allocatedOnlookerBees.get(neighborIndex);
+
+		for (int j = 0; j < currentOnlookerBee.getCurrentPosition().length; j++)
+		{
+		    currentOnlookerBee.getCurrentPosition()[j] += ThreadLocalRandom.current().nextInt(-1, 2)
+			    * (currentOnlookerBee.getCurrentPosition()[j]
+				    - neighborOnlookerBee.getCurrentPosition()[j]);
+		}
+
+		double newFitness = function.fitness(currentOnlookerBee.getCurrentPosition());
+
+		if (function.compareFitness(newFitness, currentFitness))
+		{
+		    currentOnlookerBee.setBestPosition(currentOnlookerBee.getCurrentPosition());
+		}
+
 	    }
-
-	    double newFitness = function.fitness(currentOnlookerBee.getCurrentPosition());
-
-	    if (function.compareFitness(newFitness, currentFitness))
+	    
+	    double[] bestOnlookerBeePosition = this.getBestOnlookerBeePosition(function);
+	    
+	    double bestOnlookerBeePositionFitness = function.fitness(bestOnlookerBeePosition);
+	    double bestFoodSourcePosition = function.fitness(this.bestFoodSourcePosition);
+	    
+	    if(function.compareFitness(bestOnlookerBeePositionFitness, bestFoodSourcePosition))
 	    {
-		currentOnlookerBee.setBestPosition(currentOnlookerBee.getCurrentPosition());
+		this.bestFoodSourcePosition = bestOnlookerBeePosition;
+		limitOfExploitationCycles = limitOfExploitationCyclesAux;
+	    }
+	    else
+	    {
+		limitOfExploitationCycles--;
+	    }
+	}
+    }
+
+    private double[] getBestOnlookerBeePosition(Function function)
+    {
+	double[] bestPosition = null;
+
+	for (int i = 0; i < this.allocatedOnlookerBees.size(); i++)
+	{
+	    double[] bestPositionCandidate = this.allocatedOnlookerBees.get(i).getBestPosition();
+
+	    if (bestPosition == null)
+	    {
+		bestPosition = bestPositionCandidate;
+		continue;
+	    }
+	    else
+	    {
+		double bestPositionCandidateFitness = function.fitness(bestPositionCandidate);
+		double bestPositionFitness = function.fitness(bestPosition);
+
+		if (function.compareFitness(bestPositionCandidateFitness, bestPositionFitness))
+		{
+		    bestPosition = bestPositionCandidate;
+		}
 	    }
 
 	}
+
+	return bestPosition;
     }
 
     public double[] getCurrentPosition()
@@ -70,6 +121,16 @@ public class ScoutBee
     public void setCurrentPosition(double[] currentPosition)
     {
 	this.currentPosition = currentPosition;
+    }
+
+    public double[] getBestFoodSourcePosition()
+    {
+	return bestFoodSourcePosition;
+    }
+
+    public void setBestFoodSourcePosition(double[] bestFoodSourcePosition)
+    {
+	this.bestFoodSourcePosition = bestFoodSourcePosition;
     }
 
     public List<OnlookerBee> getAllocatedOnlookerBees()
