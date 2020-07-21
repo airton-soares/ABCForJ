b881b29a79016a0f5766f9102863a03fc65dceba
Airton <airtonnsoares@gmail.com>
2020-07-21 13:50:30 -0300

    Formata código

--- abcforj/src/main/java/com/github/abcforj/Main.java
+++ abcforj/src/main/java/com/github/abcforj/Main.java
@@ -27,40 +27,40 @@ public class Main
      */
     public static void main(String[] args)
     {
-	if (args.length == 7)
-	{
-	    int beehiveSize = Integer.parseInt(args[0]);
-	    float scoutBeesProportion = Float.parseFloat(args[1]);
-	    int numberOfExplorationCycles = Integer.parseInt(args[2]);
-	    int limitOfExploitationCycles = Integer.parseInt(args[3]);
-	    double bottomDomainLimit = Double.parseDouble(args[4]);
-	    double topDomainLimit = Double.parseDouble(args[5]);
-	    int dimension = Integer.parseInt(args[6]);
+        if (args.length == 7)
+        {
+            int beehiveSize = Integer.parseInt(args[0]);
+            float scoutBeesProportion = Float.parseFloat(args[1]);
+            int numberOfExplorationCycles = Integer.parseInt(args[2]);
+            int limitOfExploitationCycles = Integer.parseInt(args[3]);
+            double bottomDomainLimit = Double.parseDouble(args[4]);
+            double topDomainLimit = Double.parseDouble(args[5]);
+            int dimension = Integer.parseInt(args[6]);
 
-	    ABC abc = new ABC(beehiveSize, scoutBeesProportion, numberOfExplorationCycles, limitOfExploitationCycles,
-		    new RotatedRastriginFunction(bottomDomainLimit, topDomainLimit), dimension);
-	    abc.doFoodSearch();
-	    
-	    double[] results = abc.getBestSolutions();
-	    double[] iteractions = new double[results.length];
+            ABC abc = new ABC(beehiveSize, scoutBeesProportion, numberOfExplorationCycles, limitOfExploitationCycles,
+                              new RotatedRastriginFunction(bottomDomainLimit, topDomainLimit), dimension);
+            abc.doFoodSearch();
 
-	    for (int i = 0; i < results.length; i++)
-	    {
-		iteractions[i] = i + 1;
-	    }
+            double[] results = abc.getBestSolutions();
+            double[] iteractions = new double[results.length];
 
-	    Plot2DPanel plot = new Plot2DPanel();
-	    plot.addLinePlot("Optimization result", iteractions, results);
-	    JFrame frame = new JFrame("Optimization report");
-	    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-	    frame.setContentPane(plot);
-	    frame.pack();
-	    frame.setVisible(true);
-	}
-	else
-	{
-	    System.err.println("INVALID NUMBER OF ARGUMENTS!");
-	}
+            for (int i = 0; i < results.length; i++)
+            {
+                iteractions[i] = i + 1;
+            }
+
+            Plot2DPanel plot = new Plot2DPanel();
+            plot.addLinePlot("Optimization result", iteractions, results);
+            JFrame frame = new JFrame("Optimization report");
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+            frame.setContentPane(plot);
+            frame.pack();
+            frame.setVisible(true);
+        }
+        else
+        {
+            System.err.println("INVALID NUMBER OF ARGUMENTS!");
+        }
     }
 }
